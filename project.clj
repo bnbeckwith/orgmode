d76fc5033dc1091e5a3d9f3ccc8671903fd57eb0
Benjamin Beckwith <bnbeckwith@gmail.com>
2013-08-22 14:47:00 -0400

    Updated revision because of bug fixes and dependancy updates

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.bnbeckwith/orgmode "0.7.0"
+(defproject com.bnbeckwith/orgmode "0.7.2"
   :description "Org-mode parser"
   :url "http://github.com/bnbeckiwith/orgmode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hiccup "1.0.2"]
-                 [org.apache.commons/commons-lang3 "3.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.4"]
+                 [org.apache.commons/commons-lang3 "3.1"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
